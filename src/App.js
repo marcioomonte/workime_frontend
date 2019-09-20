4713eee16f6e0f5003877ba99dccecf9a386f356
Caio Rodrigues <rfcaio90@gmail.com>
2019-09-20 20:39:25 -0300

    cria o componente `Login`

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Dashboard from './components/Dashboard'
+import Login from './components/Login'
 
 const App = () => {
   return (
     <Router>
+      <Route exact component={Login} path="/" />
       <Route component={Dashboard} path="/dashboard" />
     </Router>
   )
---
+++ src/components/Login/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Typography } from 'antd'
+
+const Login = () => {
+  return <Typography.Title>Login page</Typography.Title>
+}
+
+export default Login
