2fb4c34a477bb75a3040a781e11efd9914689848
Caio Rodrigues <rfcaio90@gmail.com>
2019-09-20 13:51:45 -0300

    configura o ambiente e remove os arquivos desnecessários

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Typography } from 'antd'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Typography.Title type="primary">Workime app</Typography.Title>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
