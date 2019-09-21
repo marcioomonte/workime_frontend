b81d67f46fe8605c54c608e670ba326be4180ade
Caio Rodrigues <rfcaio90@gmail.com>
2019-09-21 12:23:36 -0300

    adiciona os primeiros itens da sidebar

--- src/components/UI/Sidebar/index.js
+++ src/components/UI/Sidebar/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { Layout } from 'antd'
+import { Icon, Layout, Menu } from 'antd'
 
 const Sidebar = () => {
   const { Sider } = Layout
 
   return (
-    <Sider collapsed></Sider>
+    <Sider collapsed>
+      <Menu theme="dark">
+        <Menu.Item key="1">
+          <Icon type="pie-chart" />
+          <span>Dashboard</span>
+        </Menu.Item>
+
+        <Menu.Item key="2">
+          <Icon type="user" />
+          <span>User</span>
+        </Menu.Item>
+      </Menu>
+    </Sider>
   )
 }
 
