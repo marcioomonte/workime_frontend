1157242d151a1375b3bc401f0b1841af1eacd29c
Caio Rodrigues <rfcaio90@gmail.com>
2019-09-21 12:27:14 -0300

    cria o componente `User`

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Dashboard from './components/Dashboard'
 import Login from './components/Login'
+import User from './components/User'
 
 const App = () => {
   return (
     <Router>
       <Route exact component={Login} path="/" />
       <Route component={Dashboard} path="/dashboard" />
+      <Route component={User} path="/user" />
     </Router>
   )
 }
---
+++ src/components/User/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Typography } from 'antd'
+
+import WorkimeLayout from '../UI/Layout'
+
+const User = () => {
+  return (
+    <WorkimeLayout>
+      <Typography.Title type="primary">User</Typography.Title>
+    </WorkimeLayout>
+  )
+}
+
+export default User
