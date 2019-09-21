3b20cb2e401fdaaaa2a39d9d34339ce29bff2707
Caio Rodrigues <rfcaio90@gmail.com>
2019-09-21 12:07:14 -0300

    cria a página de login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,8 +1,52 @@
 import React from 'react'
-import { Typography } from 'antd'
+import { Button, Card, Form, Input } from 'antd'
 
-const Login = () => {
-  return <Typography.Title>Login page</Typography.Title>
+const Login = ({ form }) => {
+  const { getFieldDecorator, validateFields } = form
+
+  const login = event => {
+    event.preventDefault()
+    validateFields((errors, data) => {
+      !errors && console.log(JSON.stringify(data, null, 2))
+    })
+  }
+
+  // TODO: improve layout
+  return (
+    <div style={{
+      alignItems: 'center',
+      display: 'flex',
+      height: '100vh',
+      justifyContent: 'center',
+      width: '100vw'
+    }}>
+      <Card style={{ width: '35%' }} title="Welcome to Workime">
+        <Form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={login}>
+          <Form.Item label="Username">
+            {
+              getFieldDecorator('username', {
+                rules: [{ message: 'Required field.', required: true }]
+              })(
+                <Input />
+              )
+            }
+          </Form.Item>
+
+          <Form.Item label="Password">
+            {
+              getFieldDecorator('password', {
+                rules: [{ message: 'Required field.', required: true }]
+              })(
+                <Input type="password" />
+              )
+            }
+          </Form.Item>
+
+          <Button htmlType="submit" type="primary" style={{ alignSelf: 'center' }}>Send</Button>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
-export default Login
+export default Form.create({ name: 'login-form' })(Login)
